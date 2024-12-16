3be2a7959f78ca989da93028812ef7fdb35b3106
Rishabh7778 <saty665566@gmai.com>
2024-12-16 22:30:41 +0530

    change js

--- js/app.js
+++ js/app.js
@@ -6,36 +6,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function scrollText() {
         if (scrollTop >= scrollHeight) {
-            scrollTop = 0; // Reset to the start when it reaches the bottom
+            scrollTop = 0; 
         }
         scrollable.scrollTop = scrollTop;
         scrollTop++;
     }
 
     function startScrolling() {
-        scrolling = setInterval(scrollText, 30); // Adjust the speed by changing the interval time
+        scrolling = setInterval(scrollText, 30);
     }
 
     function stopScrolling() {
         clearInterval(scrolling);
     }
 
-    // Start scrolling when the page loads
     startScrolling();
-
-    // Stop scrolling when the mouse enters the scrollable area
     scrollable.addEventListener('mouseover', stopScrolling);
-
-    // Resume scrolling when the mouse leaves the scrollable area
     scrollable.addEventListener('mouseout', startScrolling);
 });
 
 
 
-// Get all nav links
 const navLinks = document.querySelectorAll('.nav-link');
 
-// Highlight the active link based on the current URL
 navLinks.forEach(link => {
     if (link.href === window.location.href) {
         link.classList.add('active');
